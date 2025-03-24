e9c38bbd6377217db72d028b74ef8a97f3eec169
Kyler Chin <kyler@catenarymaps.org>
2025-03-24 05:13:08 -0700

    Create deutschland_local_processing.sh

---
+++ deutschland_local_processing.sh
@@ -0,0 +1,19 @@
+sudo apt install osmium-tool osmctools unzip zip -y
+wget https://github.com/catenarytransit/osm-filter/releases/download/latest/railonly-europe-latest.osm.pbf
+
+https://github.com/catenarytransit/osm-filter/releases/download/latest/pfaedle-filtered-germany-latest.osm.pbf
+
+osmconvert railonly-europe-latest.osm.pbf -o=railonly-europe-latest.osm
+osmconvert pfaedle-filtered-germany-latest.osm.pbf -o=germany-latest.osm
+
+unzip de_gtfs.zip -d de_gtfs
+      
+sed -i 's/\"\",\"Europe\/Berlin\"/\"https:\/\/catenarymaps.org\",\"Europe\/Berlin\"/g' de_gtfs/agency.txt
+        
+./pfaedle/build/pfaedle -x germany-latest.osm de_gtfs -F --inplace --mots bus,trolley-bus,trolleybus,trolley,ferry --write-colors --drop-shapes true
+
+./pfaedle/build/pfaedle -x railonly-europe-latest.osm de_gtfs -F --inplace --mots rail,metro,subway,tram,streetcar --write-colors
+
+zip -r de_gtfs_pfaedle.zip de_gtfs
+
+gh release upload latest de_gtfs_pfaedle.zip --clobber
