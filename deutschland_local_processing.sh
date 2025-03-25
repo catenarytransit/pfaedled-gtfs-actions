62b5f210d9bb6837739f61a7f5d693418b761659
Kyler Chin <kyler@catenarymaps.org>
2025-03-24 21:43:39 -0700

    Update deutschland_local_processing.sh

--- deutschland_local_processing.sh
+++ deutschland_local_processing.sh
@@ -1,4 +1,3 @@
-#sudo apt install osmium-tool osmctools unzip zip -y
 wget https://github.com/catenarytransit/osm-filter/releases/download/latest/railonly-europe-latest.osm.pbf -O railonly-europe-latest.osm.pbf
 
 wget https://github.com/catenarytransit/osm-filter/releases/download/latest/pfaedle-filtered-germany-latest.osm.pbf -O pfaedle-filtered-germany-latest.osm.pbf
@@ -12,13 +11,17 @@ rm -rf de_gtfs
 unzip de_gtfs.zip -d de_gtfs
 
 ./gtfs-de-agency-remover/target/release/gtfs-de-agency-remover de_gtfs
-      
+
 sed -i 's/\"\",\"Europe\/Berlin\"/\"https:\/\/catenarymaps.org\",\"Europe\/Berlin\"/g' de_gtfs/agency.txt
-        
+
 pfaedle -x germany-latest.osm de_gtfs -F --inplace --mots bus,trolley-bus,trolleybus,trolley,ferry --write-colors --drop-shapes true
 
 pfaedle -x railonly-europe-latest.osm de_gtfs -F --inplace --mots rail,metro,subway,tram,streetcar --write-colors --drop-shapes true
 
-zip -r de_gtfs_pfaedle.zip de_gtfs
+./shape-squash/target/release/shape-squash de_gtfs/shapes.txt
+gtfstidy -s de_gtfs/
+
+rm de_gtfs_pfaedle.zip
+zip de_gtfs_pfaedle.zip gtfs-out/*
 
 gh release upload latest de_gtfs_pfaedle.zip --clobber -R https://github.com/catenarytransit/pfaedled-gtfs-actions/
