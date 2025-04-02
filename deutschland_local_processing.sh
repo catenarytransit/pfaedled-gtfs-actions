f5fb0d63b7a0339ec4829e466a1cc524dac9e18c
Kyler Chin <kyler@catenarymaps.org>
2025-04-02 16:58:21 -0700

    Update deutschland_local_processing.sh

--- deutschland_local_processing.sh
+++ deutschland_local_processing.sh
@@ -14,9 +14,15 @@ unzip de_gtfs.zip -d de_gtfs
 
 sed -i 's/\"\",\"Europe\/Berlin\"/\"https:\/\/catenarymaps.org\",\"Europe\/Berlin\"/g' de_gtfs/agency.txt
 
-pfaedle -x germany-latest.osm de_gtfs -F --inplace --mots bus,trolley-bus,trolleybus,trolley,ferry --write-colors --drop-shapes true
+rm -rf de_gtfs_tidy/
 
-pfaedle -x railonly-europe-latest.osm de_gtfs -F --inplace --mots rail,metro,subway,tram,streetcar --write-colors --drop-shapes true
+gtfstidy --fix -s -o de_gtfs_tidy/ de_gtfs/
+
+mv de_gtfs_tidy/* de_gtfs/
+
+pfaedle -x germany-latest.osm de_gtfs -F --inplace --mots bus,trolley-bus,trolleybus,trolley,ferry --write-colors
+
+pfaedle -x railonly-europe-latest.osm de_gtfs -F --inplace --mots rail,metro,subway,tram,streetcar --write-colors
 
 ./shape-squash/target/release/shape-squash de_gtfs/shapes.txt
 #gtfstidy --fix -s de_gtfs/
