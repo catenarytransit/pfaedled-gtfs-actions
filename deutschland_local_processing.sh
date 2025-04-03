caab81992db32ae8d9a0f082659e4b393ed7b1d8
Kyler Chin <kyler@catenarymaps.org>
2025-04-02 23:44:43 -0700

    Update deutschland_local_processing.sh

--- deutschland_local_processing.sh
+++ deutschland_local_processing.sh
@@ -25,10 +25,10 @@ pfaedle -x germany-latest.osm de_gtfs -F --inplace --mots bus,trolley-bus,trolle
 pfaedle -x railonly-europe-latest.osm de_gtfs -F --inplace --mots rail,metro,subway,tram,streetcar --write-colors --drop-shapes true
 
 ./shape-squash/target/release/shape-squash de_gtfs/shapes.txt
-#gtfstidy --fix -s de_gtfs/
+gtfstidy --fix -s  -o de_gtfs_tidy/ de_gtfs/
 
 rm de_gtfs_pfaedle.zip
-zip de_gtfs_pfaedle.zip de_gtfs/*
+zip de_gtfs_pfaedle.zip de_gtfs_tidy/*
 
 gh release upload latest de_gtfs_pfaedle.zip --clobber -R https://github.com/catenarytransit/pfaedled-gtfs-actions/
 
